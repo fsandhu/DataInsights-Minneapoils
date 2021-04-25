fbd2a67dc705de96394f233f196fe70a8ba6a5a1
nithishreddy05 <79666961+nithishreddy05@users.noreply.github.com>
2021-04-25 21:50:22 +0530

    Add files via upload

---
+++ final_st_dbscan.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+
+from st_dbscan import ST_DBSCAN
+import matplotlib.pyplot as plt# reading the classic iris dataset into a df
+st_df = pd.read_csv("C://Users//KALYAN//Downloads//CSCE474Project-main//CSCE474Project-main//forceDataPreProcessed.csv")
+dataset = st_df.head(n=1000)
+print(dataset)
+dataset_table = dataset[['PoliceUseOfForceID','CaseNumber','X', 'Y']]
+print(dataset_table)
+dataset_table.columns  = ['PoliceUseOfForceID','CaseNumber','X', 'Y']
+sp_th = 200
+tem_th = 20
+min_neighbors = 3 # points
+st_dbscan_m = ST_DBSCAN((dataset_table, sp_th, tem_th, min_neighbors))
+print(st_dbscan_m)
+labels = dataset['Is911Call'].values
+X_main = dataset_table[['X', 'Y']].values
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+            # Black Used for noise. The cluster results are -1 is noise point, expressed as black
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+
+    xy = X_main[class_member_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),markeredgecolor='k', markersize=6)
+
+    plt.title('ST-DBSCAN: #n of clusters {}'.format(len(unique_labels)))
+    plt.xlabel('longitude(E)')
+    plt.ylabel('latitude(N)')
+    plt.show()
